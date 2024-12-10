aaf856363f573144d50661735a419333b2da9b3a
lizinwn <yaurisliseth@gmail.com>
2024-12-10 15:40:43 -0500

    Crear post agregado

--- src/app/paginas/feed/feed.component.ts
+++ src/app/paginas/feed/feed.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { AuthService } from '../../servicios/auth.service';
+import { CrearPostComponent } from '../crear-post/crear-post.component';
 
 @Component({
   selector: 'app-feed',
@@ -8,5 +11,15 @@ import { Component } from '@angular/core';
   styleUrl: './feed.component.css'
 })
 export class FeedComponent {
-
+  userData: any;
+  constructor(private dialog : MatDialog, private authService: AuthService){
+  
+  this.authService.userData1$.subscribe((data) => {
+    this.userData = data;
+    
+  });
+}
+  crearPost(){
+    this.dialog.open(CrearPostComponent);
+  }
 }
