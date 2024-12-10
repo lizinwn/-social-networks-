0292ff4f2f83adac1196e79f3b3ebda9db75ea59
lizinwn <yaurisliseth@gmail.com>
2024-12-10 15:26:00 -0500

    Diseño de feed agregado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { FeedComponent } from './paginas/feed/feed.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "", component: FeedComponent},
+    //{path: "perfil", component: PerfilComponent},
+    //{path: "eventos", component: EventoComponent},
+    //{path: "grupos", component: GrupoComponent},
+];
